04a1405f0f34b28026689288e4fc5d4b30b5343c
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2021-07-10 19:17:58 -0300

    test: ensure LocalLoadPurchases deletes old cache  on validate if cache is expired

--- src/data/usecases/load-purchases/local-validate-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -48,6 +48,39 @@ describe("LocalLoadPurchases", () => {
     expect(cacheStore.fetchKey).toBe('purchases');
   });
 
-  
+  test("Should delete cache if its expired", () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate);
+    timestamp.setSeconds(timestamp.getSeconds() - 1);
+    const { cacheStore,sut } = makeSut(currentDate);
+    cacheStore.fetchResult = {
+      timestamp,
+      value: mockPurchases()
+    }
+    sut.validate();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete
+    ]);
+    expect(cacheStore.fetchKey).toBe('purchases');
+    expect(cacheStore.deleteKey).toBe('purchases');
+  });
+
+  test("Should delete cache if its on expiration date", () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate)
+    const { cacheStore,sut } = makeSut(currentDate);
+    cacheStore.fetchResult = {
+      timestamp,
+      value: mockPurchases()
+    }
+    sut.validate();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete
+    ]);
+    expect(cacheStore.fetchKey).toBe('purchases');
+    expect(cacheStore.deleteKey).toBe('purchases');
+  });
   
 });
