563376162c97e3bc2ebc3780183ba75b956f0b1b
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2021-07-10 12:10:20 -0300

    test: ensure LocalLoadPurchases will not delete cache upon creation

---
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -0,0 +1,25 @@
+import { LocalLoadPurchases } from "@/data/usecases";
+import { mockPurchases, CacheStoreSpy } from "@/data/tests";
+
+type SutTypes = {
+  sut: LocalLoadPurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore);
+  return {
+    sut,
+    cacheStore,
+  };
+};
+
+describe("LocalLoadPurchases", () => {
+  test("Should not delete or insert cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+    expect(cacheStore.messages).toEqual([]);
+  });
+
+  
+});
