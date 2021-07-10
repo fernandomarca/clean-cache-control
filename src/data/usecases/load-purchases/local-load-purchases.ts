fd714a84af6ea80f770155bc939c25fcfd7654f7
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2021-07-10 18:52:34 -0300

    test: ensure LocalLoadPurchases deletes old cache if  load fails

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -29,6 +29,15 @@ class LocalLoadPurchases implements SavePurchases, LoadPurchases {
     }
 
   }
+
+   validate():void{
+    try {
+      this.cacheStore.fetch(this.key)
+    } catch (error) {
+      this.cacheStore.delete(this.key)
+    }
+
+  }
 }
 
 export { LocalLoadPurchases };
--- src/data/usecases/load-purchases/local-validate-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -21,6 +21,15 @@ describe("LocalLoadPurchases", () => {
     expect(cacheStore.actions).toEqual([]);
   });
 
-  
+  test("Should delete cache if load fails", () => {
+    const { cacheStore,sut } = makeSut();
+    cacheStore.simulateFetchError();
+    sut.validate();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete
+    ]);
+    expect(cacheStore.deleteKey).toBe('purchases')
+  });
   
 });
